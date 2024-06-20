08287fe272a5602a6184213580309e48d252970f
timoruslim <timothyniels@gmail.com>
2024-06-20 11:29:36 +0700

    hide overflow x instead of y

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${rubik.className} overflow-y-hidden`}>{children}</body>
+      <body className={`${rubik.className} overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
